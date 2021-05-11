adc2260883cd064bee9e6ba0ef413827353edac8
lokmen abid <lokmenabid@gmail.com>
2021-05-11 21:26:19 +0100

    signup

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 const data = require('./data.json');
 const Hotel = require('./db/models/Hotel')
-const db = require("./db/database");
+const db = require("./db");
 
 Hotel.insertMany(data, function (err, result) {
     if (err) throw err
